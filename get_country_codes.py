d8f39803438aed804e79a9a685c18bd904b2f383
bronsonnaylor <49175664+bronsonnaylor@users.noreply.github.com>
2019-05-01 21:48:51 -0600

    Update get_country_codes.py

--- get_country_codes.py
+++ get_country_codes.py
@@ -1,3 +1,7 @@
-def get_country_codes(param):
-    # Your code here. Maybe try pseudocoding this one out?
-    return ""
+def get_country_codes(prices):
+    list = prices.split()
+    code_list = []
+    for i in list:
+        country_code = str(i).split("$")[0]
+        code_list.append(country_code)
+    return ", ".join(code_list)
